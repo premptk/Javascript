631deefc3e427f705284abd2ad2fa75116ea59de
Prem P <prem.p.in@ad.rupeek.net>
2023-01-28 13:18:25 +0530

    Data Types of JS

---
+++ Basic/variables.js
@@ -0,0 +1,21 @@
+var name = "Prem"
+console.log(name)
+console.log("You are doing \"great\"")
+
+var x = 5
+var y = 6
+var res = x + y
+console.log(res)
+
+var d = 5.422
+console.log(d)
+
+var a;
+console.log(a)      // undefined
+var b = null;
+console.log(b)      // null
+
+var c = "hello"
+c = 4.2
+console.log(c)      // mutable in nature
+console.log(typeof(c))
